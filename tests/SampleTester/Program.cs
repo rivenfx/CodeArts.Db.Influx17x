5b41acc6b94d11373198a2825661523d90227770
staneee <yi.hang@live.com>
2021-05-28 15:14:43 +0800

    修改测试代码

--- tests/SampleTester/Program.cs
+++ tests/SampleTester/Program.cs
@@ -25,66 +25,89 @@ namespace SampleTester
                     password: string.Empty
               );
 
-            var influxDbClient = influxOptions.CreateSampleInfluxClient(
-                mapper: Influx17xEntityMaper.TimestampInstance
-                );
+            var influxDbClient = influxOptions.CreateSampleInfluxClient();
             var connectionConfig = new Influx17xConnectionConfig(
                    influxDbClient
                );
 
-            //var start1 = new DateTime(
-            //               2021,
-            //               01,
-            //               01,
-            //               01,
-            //               01,
-            //               01,
-            //               DateTimeKind.Utc
-            //               );
-            //var dataList = new List<MySensor>();
-            //for (int i = 0; i < 100; i++)
-            //{
-            //    //var pointData = PointData
-            //    //    .Measurement($"mysensor_{start1.ToString("yyyyMM")}")
-            //    //    .Tag("sensor_id", i.ToString())
-            //    //    .Field("deployment", $"point {i}")
-            //    //    .Field("data", i)
-            //    //    .TimeStamp(start1);
-            //    //;
-            //    var sensor = new MySensor()
-            //    {
-            //        SensorId = i.ToString(),
-            //        Deployment = $"point {i}",
-            //        Value = i,
-            //        Timestamp = start1
-            //    };
-
-            //    dataList.Add(sensor);
-
-            //    start1 = start1.AddDays(1);
-            //}
-            //var writeRes = influxDbClient.BatchInsertAsync(dataList)
-            //    .ConfigureAwait(false)
-            //    .GetAwaiter()
-            //    .GetResult();
-
-
+            // 初始化 CodeArts
             CodeArtsHelper.InitializeBasic();
 
+            // 初始化 CodeArts.Db.Influx17x
             Influx17xHelper.InitializeDefaultConnectionConfig(connectionConfig);
             Influx17xHelper.InitializeCodeArts();
 
+
+            // 新增数据
+            InsertData(influxDbClient);
+            // 查询数据
+            QueryData(influxDbClient);
+
+        }
+
+
+        static void InsertData(SampleInfluxClient client)
+        {
+            var writeRes = client.InsertAsync(new MySensor()
+            {
+                SensorId = "none",
+                Deployment = "point none",
+                Value = 1,
+                Timestamp = DateTime.Now
+            }).ConfigureAwait(false)
+               .GetAwaiter()
+               .GetResult();
+
+            var start1 = new DateTime(
+                           2021,
+                           01,
+                           01,
+                           01,
+                           01,
+                           01,
+                           DateTimeKind.Utc
+                           );
+            var dataList = new List<MySensor>();
+            for (int i = 0; i < 100; i++)
+            {
+                var sensor = new MySensor()
+                {
+                    SensorId = i.ToString(),
+                    Deployment = $"point {i}",
+                    Value = i,
+                    Timestamp = start1
+                };
+
+                dataList.Add(sensor);
+
+                start1 = start1.AddDays(1);
+            }
+
+            writeRes = client.BatchInsertAsync(
+                dataList, 
+                timestampAddToTableName: true
+                )
+                .ConfigureAwait(false)
+                .GetAwaiter()
+                .GetResult();
+        }
+
+        static void QueryData(SampleInfluxClient client)
+        {
             var query = Influx17xHelper
+                //.CreateDeaultQuery<MySensor>()
                 .CreateDeaultQuery<MySensor>(DateTime.Parse("2021-01-01"))
                 .Where(o => o.Value > -1)
                 //.Skip(0)
                 //.Take(10)
                 ;
 
-            //influxDbClient.Client.WriteAsync()
 
-            Console.WriteLine(query.ToString());
+            // 获取结果
             var res = query.ToList();
+
+            // 打印sql
+            Console.WriteLine(query.ToString());
         }
     }
 
